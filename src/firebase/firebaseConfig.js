bb82ead29c4d9696b6280680581c38aa4516c044
soctopus2327 <152524824+soctopus2327@users.noreply.github.com>
2024-10-03 05:37:56 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "emotion-detector-95a5c.firebaseapp.com",
     projectId: "emotion-detector-95a5c",
     storageBucket: "emotion-detector-95a5c.appspot.com",
